8035ad9bbb1f367f4d682f7341306b622fd24880
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2022-11-02 23:01:26 +1100

    Implement main structure of tokenizer.

--- IceLang/src/Parsing.nim
+++ IceLang/src/Parsing.nim
@@ -1,4 +1,5 @@
-import std/[options, unicode, strutils]
+import std/[options, unicode, strutils, strformat, parseutils]
+import sugar
 
 
 type
@@ -7,12 +8,17 @@ type
     s: string
 
   TokenKind* = enum
+    tkIdent,
     tkInt,
+    tkEqual,
     tkPlus,
-    tkDash
+    tkDash,
+    tkLet
   
   Token* = object
     case kind*: TokenKind
+    of tkIdent:
+      ident: string
     of tkInt:
       intVal*: int
     else:
@@ -31,15 +37,25 @@ func newTokenizer(source: string): Tokenizer =
   Tokenizer(source: RuneIterator(i: 0, s: source), peekedTokens: @[], previousWasNewline: true)
 
 
+func findLastWhere(r: RuneIterator, predicate: proc(c: Rune): bool): int =
+  result = r.i
+  while result < len(r.s):
+    var rune: Rune
+    fastRuneAt(r.s, result, rune)
+    if not predicate(rune):
+      dec result
+      break
+
+
 func peekChar(t: Tokenizer, k: int = 0): Rune {.inline.} =
-  if t.source.i < t.source.s.len:
+  if t.source.i < len(t.source.s):
     var r: Rune
     fastRuneAt(t.source.s, t.source.i, r, doInc=false)
     result = r
 
 
 proc nextChar(t: var Tokenizer): Option[Rune] {.inline.} =
-  if t.source.i < t.source.s.len:
+  if t.source.i < len(t.source.s):
     var r: Rune
     fastRuneAt(t.source.s, t.source.i, r)
     result = some(r)
@@ -51,15 +67,44 @@ proc skipWhitespace(t: var Tokenizer) {.inline.} =
 
 
 proc tokenizeNumber(t: var Tokenizer): Option[Token] =
-  discard
+  let startIndex = t.source.i
+  let endIndex = t.source.findLastWhere((c) => isDigit(char(c)))
+  let slice = t.source.s[startIndex..<endIndex]
+  t.source.i = endIndex
+
+  var num: int
+  discard parseInt(slice, num)
+
+  return some(Token(kind: tkInt, intVal: num))
 
 
 proc tokenizeIdentOrKeyword(t: var Tokenizer): Option[Token] =
-  discard
+  let startIndex = t.source.i
+  let endIndex = t.source.findLastWhere((c) => isAlphaNumeric(char(c)))
+  let slice = t.source.s[startIndex..<endIndex]
+  t.source.i = endIndex
+
+  let token = 
+    case slice
+    of "let":
+      Token(kind: tkLet)
+    else:
+      Token(kind: tkIdent, ident: slice)
+
+  return some(token)
 
 
 proc tokenizeOperator(t: var Tokenizer): Option[Token] =
-  discard
+  let c = t.nextChar().get
+  case c
+  of Rune('='):
+    result = some(Token(kind: tkEqual))
+  of Rune('+'):
+    result = some(Token(kind: tkPlus))
+  of Rune('-'):
+    result = some(Token(kind: tkDash))
+  else:
+    raise newException(Exception, fmt"`{c}` is not a valid operator.")
 
 
 proc nextNoPeeking(t: var Tokenizer): Option[Token] =
@@ -78,7 +123,7 @@ proc nextNoPeeking(t: var Tokenizer): Option[Token] =
 
 
 proc next(t: var Tokenizer): Option[Token] =
-  if t.peekedTokens.len != 0:
+  if len(t.peekedTokens) != 0:
     result = some(t.peekedTokens[0])
     t.peekedTokens.delete(0)
   else:
